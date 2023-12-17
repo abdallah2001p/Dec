0ffe0c74319fd04a55872d668cfad3cc08e22f90
abdallah ahmed <abdallah2001ahmedp@gmail.com>
2023-12-17 06:05:39 +0200

    hello

--- main.js
+++ main.js
@@ -35,10 +35,12 @@ function imageSilder() {
 function changeBackground() {
     for(let i of imagesName.keys())
     {
+        if(window.innerWidth >= 767) {
         if(imageDisplayed == i)
         {
                 landingPage.style.backgroundImage = `url('${imagesName.get(i)}')`
                 changeDots()
+        }
         }
     }
 }
